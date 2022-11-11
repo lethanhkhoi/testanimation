99bd1941c41fcb1c28166aee69974ffdc3d3a162
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-11-11 19:34:40 +0700

    galaxy

--- src/component/galaxy/index.jsx
+++ src/component/galaxy/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
+// import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
+// import { OrbitControls } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
 import Sidebar from "../sidebar/Sidebar";
 
 const Galaxy = () => {
